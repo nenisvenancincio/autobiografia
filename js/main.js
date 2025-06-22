bde2de0673fd18c2eabacc5cc98d279da63c965a
irenevenancio46@gmail.com <irenevenancio46@gmail.com>
2025-06-22 15:10:48 +0100

    atualização

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const searchInput = document.getElementById('search')
+
+searchInput.addEventListener('input', (event) => {
+const value = formatString(event.target.value);
+const items = document.querySelectorAll('.items .item')
+
+items.forEach(item => {
+   if(formatString(item.textContent).indexOf(value) !== -1)
+   {
+    item.style.display = 'flex';
+   } else {
+item.style.display = 'none';
+   }
+
+   
+})
+})
+
+function formatString(value) {
+    return value
+    .toLowerCase()
+    .trim();
+}
